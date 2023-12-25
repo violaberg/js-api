257de9b3dccb6a4585de9fa45c9f0aeeb39be44a
unknown <viola.bergere@gmail.com>
2023-12-25 20:25:36 +0000

    Start js

---
+++ .devcontainer/assets/js/script.js
@@ -0,0 +1,29 @@
+const API_KEY = "_6KNsuIJ_56Oo7ypJVZ2YflkXfU";
+const API_URL = "https://ci-jshint.herokuapp.com.api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data.expiry);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
